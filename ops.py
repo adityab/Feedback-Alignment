e91fc574364ed2826d322cfe348601221830f349
Aditya Bhatt <aditya@bhatts.org>
2017-02-28 19:41:36 +0100

    Expose activation operations in graph

--- ops.py
+++ ops.py
@@ -2,8 +2,12 @@ import math
 import numpy as np
 import tensorflow as tf
 
-def lrelu(x, leak=0.2, name="lrelu"):
-  return tf.maximum(x, leak*x)
+def lrelu(x, leak=0.2, name="activation"):
+  with tf.variable_scope(name):
+    f = tf.maximum(x, leak*x, name='f')
+    df = tf.truediv(f, x, name='df')
+
+    return f
 
 def fc(input_, output_size, fn=None, stddev=0.02, bias_start=0.0, name=None):
   """Fully-Connected Layer
@@ -12,12 +16,12 @@ def fc(input_, output_size, fn=None, stddev=0.02, bias_start=0.0, name=None):
 
   with tf.variable_scope(name):
     # Define Weights and Biases in layer scope
-    matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
+    weight = tf.get_variable("weight", [shape[1], output_size], tf.float32,
                  tf.random_normal_initializer(stddev=stddev))
     bias = tf.get_variable("bias", [output_size],
       initializer=tf.constant_initializer(bias_start))
 
-    act = tf.matmul(input_, matrix) + bias
+    act = tf.matmul(input_, weight) + bias
 
     # Apply activation function fn
     if fn is not None:
